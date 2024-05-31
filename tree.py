3fc838616e32b65d300eeac9063aacf327751361
MichaelKrylivskyi <mkr20111991@gmail.com>
2024-05-31 17:59:09 +0300

    working structure with os module

--- tree.py
+++ tree.py
@@ -1,45 +1,44 @@
-class Node:
+import os
 
-    def __init__(self, value):
-        self.value = value
-        self.right = None
-        self.left = None
+class Node:
+    def __init__(self, name, is_file=False):
+        self.name = name
+        self.is_file = is_file
+        self.children = {}
 
     def __str__(self):
-        return self.value
+        return self.name
 
-class BinaryTree:
+class DirectoryTree:
+    def __init__(self, root_path):
+        self.root = Node(root_path)
+        self.build_tree(root_path, self.root)
 
-    def __init__(self):
-        self.root = None
-
-    def add_node(self, value):
-        if not self.root:
-            self.root = Node(value)
+    def build_tree(self, current_path, current_node):
+        try:
+            entries = os.listdir(current_path)
+        except PermissionError:
             return
 
-        current = self.root
-        while True:
-            if current.value > value:
-                if current.left:
-                    current = current.left
-                    continue
-                else:
-                    current.left = Node(value)
-                    break
-            elif current.value < value:
-                if current.right:
-                    current = current.right
-                    continue
-                else:
-                    current.right = Node(value)
-                    break
+        for entry in entries:
+            full_path = os.path.join(current_path, entry)
+            if os.path.isdir(full_path):
+                child_node = Node(entry)
+                current_node.children[entry] = child_node
+                self.build_tree(full_path, child_node)
             else:
-                raise ValueError("Node already exists")
+                current_node.children[entry] = Node(entry, is_file=True)
+
+    def print_tree(self, node=None, indent=0):
+        if node is None:
+            node = self.root
+
+        print('    ' * indent + str(node))
+        for child in node.children.values():
+            self.print_tree(child, indent + 1)
 
 if __name__ == "__main__":
-    tree = BinaryTree()
-    tree.add_node(9)
-    tree.add_node(8)
-    tree.add_node(7)
-    tree.add_node(15)
+    root_directory = os.getcwd()  # Current working directory
+    directory_tree = DirectoryTree(root_directory)
+    print("Directory Tree of", root_directory)
+    directory_tree.print_tree()
