b93db64149cdc14089d324801c5f0dc82f012fb6
MichaelKrylivskyi <mkr20111991@gmail.com>
2024-05-31 17:02:12 +0300

    recursive binary search

--- hash.py
+++ hash.py
@@ -1,19 +1,47 @@
-NUM_BUCKETS = 4
+class OrderedList:
+    def __init__(self):
+        self.items = []
 
-def bin_hash(value):
-    return value % NUM_BUCKETS
+    def add(self, item):
+        if self.items == []:
+            self.items.append(item)
+        else:
+            for i in range(len(self.items)):
+                if item < self.items[i]:
+                    self.items.insert(i, item)
+                    break
+            else:
+                self.items.append(item)
 
+    def size(self):
+        return len(self.items)
+
+    def search(self, item):
+        return self._binary_search_recursive(item, 0, len(self.items) - 1)
+
+    def _binary_search_recursive(self, item, left, right):
+        if left > right:
+            return False
+        mid = (left + right) // 2
+        if self.items[mid] == item:
+            return True
+        elif self.items[mid] > item:
+            return self._binary_search_recursive(item, left, mid - 1)
+        else:
+            return self._binary_search_recursive(item, mid + 1, right)
+
+
+# Example usage:
 if __name__ == "__main__":
-    hash_table = [[], [], [], []]
+    ordered_list = OrderedList()
     sequence = [15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14]
     for element in sequence:
-        hash_table[bin_hash(element)].append(element)
-    print(hash_table)
+        ordered_list.add(element)
+
+    print("Ordered list:", ordered_list.items)
+
     to_find = 17
-    for element in hash_table[bin_hash(to_find)]:
-        print(element)
-        if element == to_find:
-            print("Found")
-            break
+    if ordered_list.search(to_find):
+        print(f"{to_find} found in the ordered list.")
     else:
-        print("Not found")
+        print(f"{to_find} not found in the ordered list.")
